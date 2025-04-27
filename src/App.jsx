2f97cf324afb4cf32082004de287c8e27838bda8
alexdevsprint <alexdevsprint@gmail.com>
2025-04-27 23:46:03 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,27 @@ import Description from "./components/Description/Description";
 import Options from "./components/Options/Options";
 import Feedback from "./components/Feedback/Feedback";
 
+import { useState } from 'react';
+
 function App() {
+  const [ clicks, setClicks ] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
+
+  const updateFeedback = feedbackType => {
+    // Тут використовуй сеттер, щоб оновити стан
+    console.log(feedbackType)
+    setClicks ({...clicks, 
+      [feedbackType]: clicks[feedbackType] + 1,
+  });
+   }
+
   return (
     <div className={css.container}>
       <Description />
-      <Options />
+      <Options onUpdate={updateFeedback}/>
       <Feedback />
     </div>
   );
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,10 +1,10 @@
-import css from './Options.module.css'
-function Options() {
+import css from './Options.module.css'     
+function Options({onUpdate}) {
     return (
         <div className={css.optionsContainer}>
-            <button className={css.optionsBtn} type="button">Good</button>
-            <button className={css.optionsBtn} type="button">Neutral</button>
-            <button className={css.optionsBtn} type="button">Bad</button>
+            <button className={css.optionsBtn} onClick={() => onUpdate('good')} type="button">Good</button>
+            <button className={css.optionsBtn} onClick={() => onUpdate('neutral')} type="button">Neutral</button>
+            <button className={css.optionsBtn} onClick={() => onUpdate('bad')} type="button">Bad</button>
             <button className={css.optionsBtn} type="button">Reset</button>
         </div>
     )
