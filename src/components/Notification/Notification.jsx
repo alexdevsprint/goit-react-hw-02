a0ee04427731cf31b225fda17b7f1fe7546a1777
alexdevsprint <alexdevsprint@gmail.com>
2025-04-28 14:57:26 +0300

    commit

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,8 +1,9 @@
+import css from "./Notification.module.css"
 function Notification () {
     return (
-        <>
-            No feedback yet
-        </>
+        <div>
+            <p className={css.notificationText}>No feedback yet</p>
+        </div>
     )
 }
 
